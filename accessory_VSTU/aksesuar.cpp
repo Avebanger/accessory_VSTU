e5f413735ea7a455f828d7e44ac231ba19cd27a0
Сергей <@DESKTOP-KABKHJQ>
2026-09-05 20:25:40 +0300

    Добавьте файлы проекта.

---
+++ accessory_VSTU/aksesuar.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+	puts("                  arbuz                               ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                                                 ");
+	puts("                  darova                               ");
+}
